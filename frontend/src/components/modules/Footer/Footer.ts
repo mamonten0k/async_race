173575fa3818980216c1491ff9b5e51fb7ddea25
mamonten0k <asdnbvb123@gmail.com>
2022-08-09 20:59:54 +0300

    feat: create Footer component

---
+++ frontend/src/components/modules/Footer/Footer.ts
@@ -0,0 +1,14 @@
+import './Footer.scss';
+
+const Footer = ():string => {
+	return `
+		<footer class='footer'>
+			<div class='footer__container container'>
+				<div>RSSchool, 2022</div>
+				<div>Created by mamonten0k</div>
+			</div>
+		</footer>
+	`
+}
+
+export { Footer };
