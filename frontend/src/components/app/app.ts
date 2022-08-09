a163460b986e7bb42e1c074c25a1593160d0a1ee
mamonten0k <asdnbvb123@gmail.com>
2022-08-09 21:01:42 +0300

    feat: make application render components into document

--- frontend/src/components/app/app.ts
+++ frontend/src/components/app/app.ts
@@ -1,12 +1,13 @@
+import { Default } from "../layouts/Default";
+import { Garage } from "../modules";
+
+const root: HTMLElement | null = document.getElementById('root');
+const initial: string = Garage('as');
+
 class App {
     start() {
-    fetch('http://localhost:8000/garage')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-      });
+      if(!root) return;
+      root.innerHTML = Default(initial);
     }
 }
 
@@ -16,3 +17,10 @@ export { App };
 // отрисовка - запрос к данным, отрисовка машин из гаража.
 // нажатие гонки - просто race, пока один из них не докатится, сделать анимации нормальные
 // записывать победителя post запросом, вести учет победителей, количество их побед.
+// fetch('http://localhost:8000/garage')
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         console.log(data);
+//       });
