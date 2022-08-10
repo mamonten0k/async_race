a4f4563622f656e61f6264ef8e628686223f184f
mamonten0k <asdnbvb123@gmail.com>
2022-08-10 21:14:14 +0300

    feat: create Garage component

--- frontend/src/components/modules/Garage/Garage.ts
+++ frontend/src/components/modules/Garage/Garage.ts
@@ -1,19 +1,19 @@
 import './Garage.scss';
 
-const Garage = (cars: string):string => {
+const Garage = (cars: Array<string>): string => {
 	return `
 		<div class="controls">
 			<h1 class="garage__title">Controls</h1>
 			<div class="row row-create">
-				<input class='input input-text' type="text" />
-				<input class='input input-palette' type="color" id="colorpicker" value="#0aaaae">
+				<input class='input input-text' id="create-with-name" type="text" />
+				<input class='input input-palette' id="create-with-color" type="color" id="colorpicker" value="#ff7300">
 				<button class="btn btn-control" id="btn-create">
 					Create
 				</button>
 			</div>
 			<div class="row row-update">
-				<input class='input input-text' type="text" />
-				<input class='input input-palette' type="color" id="colorpicker" value="#1e1e1">
+				<input class='input input-text' id="name-update"  type="text" />
+				<input class='input input-palette' id="color-update" type="color" id="colorpicker" value="#1e1e1e">
 				<button class="btn btn-control" id="btn-update">
 					Update
 				</button>
@@ -30,7 +30,7 @@ const Garage = (cars: string):string => {
 		</div>
 		<div class="garage">
 			<h1 class="garage__title">Garage (${localStorage.getItem('garageVol')})</h1>
-			<ul class="garage__items items"></ul>
+			<ul class="garage__items items" id='cars-enlisted'>${cars.map(car => `<li>${car}</li>`).join('')}</ul>
 		</div>
 	`
 }
