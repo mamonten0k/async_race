81132b2b00837906ea923f59eee71b50f67d824f
mamonten0k <asdnbvb123@gmail.com>
2022-08-11 14:30:02 +0300

    feat: finish carFactory create, update and reset methods

--- frontend/src/utils/CarFactory.ts
+++ frontend/src/utils/CarFactory.ts
@@ -1,4 +1,6 @@
-import { Track } from '../components/base/'
+import { Track } from '../components/base/';
+import { handleSelect, handleRemove } from './EventHandlers';
+import { getCars } from './restfulApiHandlers';
 
 interface IFactory {
 	parentNode: HTMLElement | null;
@@ -8,6 +10,12 @@ interface IFactory {
 	factoryUpdate: HTMLElement | null;
 	factoryUpdateName: HTMLElement | null;
 	factoryUpdateColor: HTMLElement | null;
+	carsTotal : HTMLElement | null;
+	garageRestet : HTMLElement | null;
+
+	start: () => void;
+	update: () => void;
+	create: () => void;
 }
 
 class CarFactory implements IFactory {
@@ -21,6 +29,9 @@ class CarFactory implements IFactory {
 	factoryUpdateName: HTMLInputElement | null;
 	factoryUpdateColor: HTMLInputElement | null;
 
+	carsTotal : HTMLElement | null;
+	garageRestet : HTMLElement | null;
+
 	constructor() {
 		this.parentNode = document.getElementById("cars-enlisted");
 
@@ -31,30 +42,68 @@ class CarFactory implements IFactory {
 		this.factoryCreateColor = document.getElementById('create-with-color') as HTMLInputElement;
 
 		this.factoryUpdate = document.getElementById('btn-update');
-		this.factoryUpdateName = document.getElementById('create-with-name') as HTMLInputElement;
-		this.factoryUpdateColor = document.getElementById('create-with-color') as HTMLInputElement;
+		this.factoryUpdateName = document.getElementById('name-update') as HTMLInputElement;
+		this.factoryUpdateColor = document.getElementById('color-update') as HTMLInputElement;
+
+		this.carsTotal = document.querySelector('.garage__title') as HTMLElement;
+		this.garageRestet = document.getElementById('btn-reset') as HTMLElement;
 	}
 
 	start() {
 		this.factoryCreate?.addEventListener('click', this.create);
 		this.factoryUpdate?.addEventListener('click', this.update);
+
+		this.reset();
+		if(this.carsTotal) this.carsTotal.innerHTML = `Garage (${localStorage.getItem('garageVol')})`
+
+		this.garageRestet?.addEventListener('click', this.reset);
 	}
 
 	create = () => {
-		const carNode = document.createElement('li');
+		const carNode : HTMLElement = document.createElement('li');
 		const carTrack = Track(this.factoryCreateColor?.value, this.factoryCreateName?.value);
+		const volTotal = +(localStorage.getItem('garageVol') || "0");
+
 		carNode.innerHTML = carTrack;
-		this.parentNode?.appendChild(carNode);
+
+		if(volTotal === 0) {
+			this.parentNode?.innerHTML = "";	
+		}
+
+		this.parentNode?.appendChild(carNode);	
+
+		const carSelect = carNode.querySelector('.btn-select') as HTMLElement;
+		const carRemove = carNode.querySelector('.btn-remove') as HTMLElement;
+		
+		carSelect.addEventListener('click', handleSelect);
+		carRemove.addEventListener('click', handleRemove);
+
+
+		localStorage.setItem('garageVol',`${volTotal + 1}`);
+		if(this.carsTotal) this.carsTotal.innerHTML = `Garage (${volTotal + 1})`;
 	}
 
 	update = () => {
-		const ref = localStorage.getItem('selected');
+		const currID = localStorage.getItem('selected');
 
-		ref.getElementById('car-name').innerHTML = this.factoryUpdateName?.value;
+		if(!currID) return;
+		
+		const currEl = document.getElementById(currID) as HTMLElement;
+		const currMark = currEl?.querySelector('.track__id') as HTMLInputElement;
+		const currColor = currEl?.querySelector('.car__icon') as HTMLInputElement;
+
+		if(this.factoryUpdateName?.value && currMark) currMark.innerHTML = `<b>${this.factoryUpdateName?.value}</b>`;
+		if(this.factoryUpdateColor?.value && currColor) currColor.style.fill = this.factoryUpdateColor?.value;
 	}
 
-	shutown = () => {
-		document.removeEventListener('click', this.create);
+	reset = () => {
+		this.parentNode?.innerHTML = `
+				<li style='color: gray; font-size: 1.1rem; padding-top: 1rem;'>
+					There are no Cars left in your Garage
+				</li>`;
+
+		localStorage.setItem('garageVol',`0`);
+		if(this.carsTotal) this.carsTotal.innerHTML = `Garage (0)`;
 	}
 }
 
